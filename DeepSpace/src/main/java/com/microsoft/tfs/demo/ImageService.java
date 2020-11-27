668536b3488928aedc093b4435371552cf9139a6
Srivatsa Marichi <srivatsams@yahoo.com>
2020-11-27 23:09:51 +0530

    removed junk

--- DeepSpace/src/main/java/com/microsoft/tfs/demo/ImageService.java
+++ DeepSpace/src/main/java/com/microsoft/tfs/demo/ImageService.java
@@ -1,31 +1,3 @@
-/**
- * ------------------------------------------ START OF LICENSE -----------------------------------------
- *
- * Deep-Space
- *
- * Copyright (c) Microsoft Corporation
- *
- * All rights reserved.
- *
- * MIT License
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
- * associated documentation files (the ""Software""), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial
- * portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
- * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * ----------------------------------------------- END OF LICENSE ------------------------------------------
- */
 package com.microsoft.tfs.demo;
 
 import javax.ws.rs.GET;
@@ -49,7 +21,7 @@ public class ImageService {
         //http://creativecommons.org/licenses/by/3.0/
         
         Image mercury = new Image();
-        mercury.setSrc("./images/mercury.png");
+        mercury.setSrc("./webapp/images/mercury.png");
         mercury.setWidth(1950);
         mercury.setHeight(1950);
         planetaries.add(mercury);
@@ -121,9 +93,7 @@ public class ImageService {
         sun.setSrc("./images/sun.png");
         sun.setWidth(660);
         sun.setHeight(660);
-        planetaries.add(sun);
-
-        
+        planetaries.add(sun);        
     }
 
     @GET
--- DeepSpace/src/test/java/com/microsoft/tfs/demo/ImageServiceTest.java
+++ DeepSpace/src/test/java/com/microsoft/tfs/demo/ImageServiceTest.java
@@ -1,31 +1,3 @@
-/**
- * ------------------------------------------ START OF LICENSE -----------------------------------------
- *
- * Deep-Space
- *
- * Copyright (c) Microsoft Corporation
- *
- * All rights reserved.
- *
- * MIT License
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
- * associated documentation files (the ""Software""), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial
- * portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
- * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * ----------------------------------------------- END OF LICENSE ------------------------------------------
- */
 package com.microsoft.tfs.demo;
 
 import com.microsoft.tfs.demo.ImageService;
@@ -53,7 +25,7 @@ public class ImageServiceTest {
 
         // Only have one of the two lines below uncommented one at a time
         //assertEquals(3, images.size());   // Leave this line in for no Earth
-        //assertEquals(4, images.size()); // Uncomment this line for Earth
+        assertEquals(4, images.size()); // Uncomment this line for Earth
     }
 
     @Test
